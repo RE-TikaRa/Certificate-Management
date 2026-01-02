9a7f3c69f979f689a174e70d652f5117e324fdc1
RE-TikaRa <154001438+RE-TikaRa@users.noreply.github.com>
2026-01-03 00:41:39 +0800

    Refactor: Clean up WebPage branch to only contain website files

--- src/ui/widgets/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-UI Widgets
-"""
-
-from .attachment_table_view import AttachmentTableView
-from .major_search import MajorSearchWidget
-from .school_search import SchoolSearchWidget
-
-__all__ = ["AttachmentTableView", "MajorSearchWidget", "SchoolSearchWidget"]
